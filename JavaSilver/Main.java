bb9bb52acabf2d0543e0e890831b93132201185c
nekkoubi-rgb <nekko.ubi@gmail.com>
2025-12-27 11:39:23 +0900

    java導入、動作確認 (#7)
    試しにsquash and merge

---
+++ JavaSilver/Main.java
@@ -0,0 +1,14 @@
+package JavaSilver;
+
+public class Main {
+    public static void main(String[] args) {
+    // 画面に文字を出す
+    System.out.println("Hello, World!");
+
+    // 簡単な計算
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    System.out.println("Sum: " + sum);
+    }
+}
