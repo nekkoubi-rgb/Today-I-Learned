c0a02e12e2931dfb1bc6b51e2c60e78450732a95
nekkoubi-rgb <nekko.ubi@gmail.com>
2025-12-30 15:26:47 +0900

    AntiGravityお試し

---
+++ JavaSilver/AntigravityTest.java
@@ -0,0 +1,10 @@
+public class AntigravityTest {
+    public static void main(String[] args) {
+        System.out.println("答えは:" + 10 + 20);
+        /*
+         * (String)答えは: + (int)10 + (int)20
+         * → (String)答えは:10 + (int)20
+         * → (String)答えは:1020
+         */
+    }
+}
